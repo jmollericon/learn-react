5b7be497ec2fd1215cb29b819478548868559dcf
Jorge Mollericon <jlmollericon@gmail.com>
2021-01-08 11:45:31 -0400

    using prop-types -> propTypes and defaultProps ok

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 const Card = ({title, image, price}) => (
   <>
@@ -7,5 +8,14 @@ const Card = ({title, image, price}) => (
     <button>{`${price} USD`}</button>
   </>
 )
-
+Card.propTypes = {
+  title: PropTypes.string,
+  image: PropTypes.string,
+  price: PropTypes.number
+}
+Card.defaultProps = {
+  title: "No se encontró el título",
+  image: "https://www.degustapanama.com/img/pix/restaurante.fonda-lo-que-hay_1_foto-r-0.jpg",
+  price: 0
+}
 export default Card
